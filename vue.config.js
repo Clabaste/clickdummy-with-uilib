1ccc7ab57e581a187ab421945ec655c3044b7cb9
Claudia Bastert <bastert@mediaworx.com>
2022-06-29 13:30:45 +0200

    use RemovePlugin for Webpack

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  transpileDependencies: true,
-});
+const RemovePlugin = require("remove-files-webpack-plugin");
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      new RemovePlugin({
+        watch: {
+          include: ["./node_modules/de-zurich-z21b-ui-library/dist/components"],
+        },
+      }),
+    ],
+  },
+};
